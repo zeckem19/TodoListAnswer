0ece7dd7b86e0facffc85cdcc012aea4d1eaa8e9
Tan Qin Hui <destqh@hotmal.com>
2019-10-23 14:28:17 +0800

    architecture

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import Command.Command;
+import Command.ExitCmd;
+
+public class Main {
+	public static void main(String[] args) {
+		Command userCmd = null;
+		Scanner sc = new Scanner(System.in);
+		
+		while(!(userCmd instanceof ExitCmd)) {
+			String input = sc.nextLine();
+			userCmd = getCommand(input);
+			userCmd.execute();
+		}
+	}
+	
+	
+	private static Command getCommand(String input) {
+		String firstWord = input.split(" ")[0];
+		
+		switch(firstWord) {
+			case "exit":
+				return (Command) new ExitCmd();
+			default:
+				return null;
+		}
+	}
+}
