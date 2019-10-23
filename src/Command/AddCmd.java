e02c375229b195e0d67cb7dccc30bcb02ca938b6
zeckem19 <zeckem19@gmail.com>
2019-10-23 15:29:06 +0800

    added AddCmd

--- src/Command/AddCmd.java
+++ src/Command/AddCmd.java
@@ -1,17 +1,21 @@
 package Command;
 
 import core.ToDoList;
+import core.Task;
 
 public class AddCmd implements Command {
-
+	public String desc;
 	@Override
 	public void setArgs(String... args) {
 		// Do Nothing
+		desc = args[0];
 	}
 
 	@Override
-	public void execute(ToDoList tdl) {
-		// TODO Auto-generated method stub
-		
+	public void execute(ToDoList myTDL) {
+		Task task = new Task(desc);
+		myTDL.addTask(task);
 	}
+	
+
 }
