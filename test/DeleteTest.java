6fed1b3124de5e979a2cdcaaf5f201a2b526a409
zacwy <zacwy@192.168.43.236>
2019-10-23 15:38:05 +0800

    fixed test for delete

--- test/DeleteTest.java
+++ test/DeleteTest.java
@@ -2,10 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import Command.AddCmd;
 import Command.Command;
-import Command.ExitCmd;
-import Command.ViewCmd;
 import Command.DeleteCmd;
 
 
@@ -15,18 +12,9 @@ public class DeleteTest {
 	public void testGetCommand() {
 		Command userCmd = Main.getCommand("unknown");
 		assertNull(userCmd);
-		
-		userCmd = Main.getCommand("exit");
-		assertTrue(userCmd instanceof ExitCmd);
-		
-		userCmd = Main.getCommand("add");
-		assertTrue(userCmd instanceof AddCmd);
-		
-		userCmd = Main.getCommand("view");
-		assertTrue(userCmd instanceof ViewCmd);
-		
+
 		userCmd = Main.getCommand("delete");
-		assertTrue(userCmd instanceof DeleteTest);
+		assertTrue(userCmd instanceof DeleteCmd);
 	}
 
 }
